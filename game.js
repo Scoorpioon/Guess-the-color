64651015f3d5af8ff0d0c475478cfbc5815aef58
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2023-02-22 13:49:57 -0300

    Finalização de código

--- game.js
+++ game.js
@@ -1,7 +1,12 @@
 let gameDifficulty = 'easy';
 const squares = document.querySelectorAll('.square');
 const squareList = document.getElementById('squares');
-let numberOfSquares = 3;
+const header = document.querySelector('.header')
+const navButtons = document.querySelectorAll('.navButton')
+const tryAgain = document.getElementById('try-again');
+const correct = document.getElementById('correct');
+const options = document.getElementById('options');
+const difficulty = document.querySelector('.difficulty')
 let counter = 0;
 
 // Gerar um número inteiro aleatório
@@ -14,20 +19,31 @@ const generateRGB = () => {
     return `rgb(${randomInt(255)}, ${randomInt(255)}, ${randomInt(255)})`;
 }
 
-// Gerar as cores aleatórias e a cor principal a ser adivinhada
-const generateColors = (difficulty) => {
+// Gerar as cores aleatórias e a cor principal a ser adivinhada (reinicia o jogo)
+const generateColors = (difficulty, getColor) => {
     const mainColor = generateRGB();
     const colors = [generateRGB(), generateRGB(), generateRGB(), generateRGB(), generateRGB()]
     let randomColor = generateRGB();
     let counter = 0;
 
+    if(getColor) {
+        return mainColor;
+    }
+
+    tryAgain.style.display = 'none'; // O botão de tentar novamente se mantém escondido até que o usuário erre pelo menos um quadrado.
+    correct.style.display = 'none' // Esconder o sinal de correto.
+
     // Remover um choosen passado para que um novo possa ser implementado.
     for(element of squares) {
+        element.style.opacity = 1;
+
         if(element.classList.contains('choosen')) {
-            element.classList.remove('choosen')
+            element.classList.remove('choosen');
+            element.classList.remove('got-it')
         }
     }
-  
+
+    // Se a dificuldade do jogo ser fácil, ele vai escolher apenas 3 quadrados para sortear. Do contrário, 6 serão sorteados.
     difficulty === 'easy' ? choosenSquare = randomInt(3) : choosenSquare = randomInt(6);
     squares[choosenSquare].classList.add('choosen');
 
@@ -42,28 +58,71 @@ const generateColors = (difficulty) => {
         }
     };
 
+    // Alterar a representação RGB do painel principal
     const colorDisplay = document.getElementById('color-display');
     colorDisplay.textContent = mainColor;
+
+    // Aplicar uma cor aleatória para os componentes livres
+    header.style.backgroundColor = randomColor
+    navButtons.forEach(element => {element.style.color = randomColor})
+
+    return mainColor;
+};
+
+// Reiniciar quadrados
+const resetSquares = () => {
+    for(square of squares) {
+        square.style.visibility = 'visible'
+    }
 };
 
 // Função main
 const startGame = () => {
     let gameDifficulty = undefined;
+    var itsRight = undefined;
     const squareList = document.getElementById('squares');
     const difficultyButtons = document.querySelectorAll('.difficulty-button');
     const startAgain = document.getElementById('start-again');
-    let attempt = 0;
+
+    let mainColor = generateColors(gameDifficulty);
 
     startAgain.addEventListener('click', () => {
-        generateColors(gameDifficulty)
+        mainColor = generateColors(gameDifficulty)
+        resetSquares();
+    })
+
+    tryAgain.addEventListener('click', () => {
+        mainColor = generateColors(gameDifficulty)
+        resetSquares();
     })
 
     squares.forEach(element => {element.addEventListener('click', (element) => {
-        let itsRight = undefined;
         element.target.classList.contains('choosen') ? itsRight = true : itsRight = false;
 
         if(itsRight) {
-            console.log('The user choosen the correct color.')
+            console.log('O usuário acertou a cor!');
+            element.target.classList.add('got-it'); // Adicionar animação quando o usuário acertar
+            correct.style.display = 'block'; // Aparecer o sinal de correto
+            tryAgain.style.display = 'none';
+            header.style.backgroundColor = mainColor;
+            
+            for(element of options.children) {
+                element.style.color = mainColor;
+            }
+            
+            for(element of difficulty.children) {
+                element.style.color = mainColor;
+            }
+
+            for(square of squares) {
+                square.style.visibility = 'visible', square.style.backgroundColor = mainColor;
+            }
+        }
+
+        if(!itsRight) {
+            console.log('Botão de tentar novamente acionado.')
+            tryAgain.style.display = 'block';
+            element.target.style.visibility = 'hidden';
         }
     })})
 
@@ -71,7 +130,8 @@ const startGame = () => {
         element.addEventListener('click', (element) => {
             if(element.target.classList.contains('easy')) {
                 gameDifficulty = 'easy';
-                generateColors(gameDifficulty);
+                mainColor = generateColors(gameDifficulty);
+                resetSquares();
                 for(element of squareList.children) {
                     if(element.classList.contains('4') || element.classList.contains('5') || element.classList.contains('6')) {
                         element.style.display = 'none';
@@ -79,7 +139,8 @@ const startGame = () => {
                 }
             } else if(element.target.classList.contains('hard')) {
                 gameDifficulty = 'hard';
-                generateColors(gameDifficulty);
+                mainColor = generateColors(gameDifficulty);
+                resetSquares();
                 for(element of squareList.children) {
                     if(element.classList.contains('4') || element.classList.contains('5') || element.classList.contains('6')) {
                         element.style.display = 'block';
@@ -88,8 +149,6 @@ const startGame = () => {
             }
         })
     })
-
-    generateColors(gameDifficulty)
 };
 
 startGame();
